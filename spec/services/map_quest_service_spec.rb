0c95cc5f081cd9871d2c8aba0db59a0373f59c52
Sienna Kopf <princess.kopf@gmail.com>
2020-09-19 08:20:16 -0600

    Add test for MapQuest Service

---
+++ spec/services/map_quest_service_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe MapQuestService do
+  context 'instance methods' do
+    context '#lat_long' do
+      it "takes in a location and transforms it into lat and long coordinates" do
+        location = "denver, co"
+        service = MapQuestService.new
+        response = service.lat_long(location)
+        expect(response).to be_a Hash
+        expect(response[:results]).to be_an Array
+        target_result = response[:results][0]
+        expect(target_result).to have_key :locations
+        expect(target_result[:locations]).to be_an Array
+        target_location = target_result[:locations][0]
+        expect(target_location).to have_key :latLng
+        expect(target_location[:latLng]).to have_key :lat
+        expect(target_location[:latLng]).to have_key :lng
+
+        expect(target_location[:latLng][:lat]).to be_a Float
+        expect(target_location[:latLng][:lng]).to be_a Float
+
+        expect(target_location[:latLng][:lat]).to eq(39.738453)
+        expect(target_location[:latLng][:lng]).to eq(-104.984853)
+      end
+    end
+  end
+end
