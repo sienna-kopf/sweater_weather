2c6637b88d689062ac5d79fc6e9f26291f8cf77f
Sienna Kopf <princess.kopf@gmail.com>
2020-09-19 09:35:52 -0600

    Build out facade for properly handling weather data:

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,36 @@
+class ForecastFacade
+  attr_reader :lat,
+              :lon
+
+  def initialize(lat, lon)
+    @lat = lat
+    @lon = lon
+    @service = WeatherService.new
+  end
+
+  def response_forecast
+    @service.forecast(@lat, @lon)
+  end
+
+  def current_forecast
+    current_forecast_info = response_forecast[:current]
+
+    CurrentForecast.new(current_forecast_info)
+  end
+
+  def hourly_forecast
+    hourly_forecast_info = response_forecast[:hourly]
+
+    hourly_forecast_info.map do |hourly_weather|
+      HourlyForecast.new(hourly_weather)
+    end.take(8)
+  end
+
+  def daily_forecast
+    daily_forecast_info = response_forecast[:daily]
+
+    daily_forecast_info.map do |daily_weather|
+      DailyForecast.new(daily_weather)
+    end.take(6)
+  end
+end
