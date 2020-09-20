cc8f9c44c5fd0daf64744aa44336af19f2021351
Sienna Kopf <princess.kopf@gmail.com>
2020-09-20 16:18:49 -0600

    Remove commented out lines from forecast serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,33 +1,4 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
   attributes :current_forecast, :hourly_forecast, :daily_forecast
-
-  # def initialize(current_forecast, hourly_forecast, daily_forecast)
-  #   @current_forecast = current_forecast
-  #   @hourly_forecast = hourly_forecast
-  #   @daily_forecast = daily_forecast
-  # end
-  #
-  # def data_hash
-  #   {
-  #     data:
-  #     {
-  #       current_forecast:
-  #       {
-  #         type: 'forecast',
-  #         attributes: @current_forecast.as_json
-  #       },
-  #       hourly_forecast:
-  #       {
-  #         type: 'forecast',
-  #         attributes: @hourly_forecast.as_json
-  #       },
-  #       daily_forecast:
-  #       {
-  #         type: 'forecast',
-  #         attributes: @daily_forecast.as_json
-  #       }
-  #     }
-  #   }
-  # end
 end
