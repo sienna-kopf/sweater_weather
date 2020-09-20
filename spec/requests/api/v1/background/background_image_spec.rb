80bb5008f53750afd0bba24666922bbce4dfdf68
Sienna Kopf <princess.kopf@gmail.com>
2020-09-20 13:14:57 -0600

    Build out test with proper structure and test for attributes NOT present:

--- spec/requests/api/v1/background/background_image_spec.rb
+++ spec/requests/api/v1/background/background_image_spec.rb
@@ -8,9 +8,47 @@ RSpec.describe "Background Image Endpoint", type: :request do
       get "/api/v1/backgrounds?location=#{location}"
 
       expect(response).to be_successful
-      # response.content_type == "application/json"
+      response.content_type == "application/json"
 
-      # background_image = JSON.parse(response.body, symbolize_names: true)
+      background_image = JSON.parse(response.body, symbolize_names: true)
+
+      expect(background_image).to be_a Hash
+      expect(background_image).to have_key :data
+      expect(background_image[:data]).to have_key :type
+      expect(background_image[:data]).to have_key :location
+      expect(background_image[:data]).to have_key :image
+
+      expect(background_image[:data][:type]).to eq("image")
+      expect(background_image[:data][:location]).to eq("denver, co")
+
+      expect(background_image[:data][:image]).to be_a Hash
+      expect(background_image[:data][:image]).to have_key :attributes
+      expect(background_image[:data][:image][:attributes]).to be_a Hash
+
+      expect(background_image[:data][:image][:attributes][:id]).to be_a Integer
+      expect(background_image[:data][:image][:attributes]).to have_key :image_url
+      expect(background_image[:data][:image][:attributes][:image_url]).to be_a String
+      expect(background_image[:data][:image][:attributes]).to have_key :user_name
+      expect(background_image[:data][:image][:attributes][:user_name]).to be_a String
+      expect(background_image[:data][:image][:attributes]).to have_key :user_url
+      expect(background_image[:data][:image][:attributes][:user_url]).to be_a String
+
+      expect(background_image[:data][:image][:attributes]).to_not have_key :type
+      expect(background_image[:data][:image][:attributes]).to_not have_key :tags
+      expect(background_image[:data][:image][:attributes]).to_not have_key :previewURL
+      expect(background_image[:data][:image][:attributes]).to_not have_key :previewWidth
+      expect(background_image[:data][:image][:attributes]).to_not have_key :previewHeight
+      expect(background_image[:data][:image][:attributes]).to_not have_key :webformatWidth
+      expect(background_image[:data][:image][:attributes]).to_not have_key :webformatHeight
+      expect(background_image[:data][:image][:attributes]).to_not have_key :largeImageURL
+      expect(background_image[:data][:image][:attributes]).to_not have_key :imageWidth
+      expect(background_image[:data][:image][:attributes]).to_not have_key :imageHeight
+      expect(background_image[:data][:image][:attributes]).to_not have_key :imageSize
+      expect(background_image[:data][:image][:attributes]).to_not have_key :views
+      expect(background_image[:data][:image][:attributes]).to_not have_key :favorites
+      expect(background_image[:data][:image][:attributes]).to_not have_key :likes
+      expect(background_image[:data][:image][:attributes]).to_not have_key :comments
+      expect(background_image[:data][:image][:attributes]).to_not have_key :user_id
     end
   end
 end
