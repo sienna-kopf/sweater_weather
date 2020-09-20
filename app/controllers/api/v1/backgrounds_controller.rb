e5476a4abd7bea1383808024a23059b8883c0333
Sienna Kopf <princess.kopf@gmail.com>
2020-09-20 13:04:49 -0600

    Serializer added. 1b complete

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
-    location = params[:location].split(',')[0]
-
-    image_facade = ImageFacade.new(location)
-
-    render json: BackgroundsSerializer.new(image_facade).data_hash
+    location = params[:location]
+    city = location.split(',')[0]
+    image = BackgroundImageFacade.new(city).background_image
+    render json: BackgroundsSerializer.new(image, location).data_hash
   end
 end
