941033bcfde05f595ec034781c829862dd57d639
Sienna Kopf <princess.kopf@gmail.com>
2020-09-21 11:18:05 -0600

    Convert temperature attribute into a string for current forecast condensed poro

--- app/poros/current_forecast_condensed.rb
+++ app/poros/current_forecast_condensed.rb
@@ -3,7 +3,7 @@ class CurrentForecastCondensed
               :summary
 
   def initialize(current_forecast_info)
-    @temperature = current_forecast_info[:temp].round(0)
+    @temperature = current_forecast_info[:temp].round(0).to_s
     @summary = current_forecast_info[:weather][0][:description]
   end
 end
