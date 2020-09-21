4765a8bc376f2304330a4ff6297d38dce2084451
Sienna Kopf <princess.kopf@gmail.com>
2020-09-21 10:42:55 -0600

    Add another instance variable to the climbing routes facade for location and pass it to the route poro when creating a route obj

--- app/facades/climbing_routes_facade.rb
+++ app/facades/climbing_routes_facade.rb
@@ -1,10 +1,12 @@
 class ClimbingRoutesFacade
   attr_reader :lat,
-              :lon
+              :lon,
+              :location
 
-  def initialize(lat, lon)
+  def initialize(lat, lon, location)
     @lat = lat
     @lon = lon
+    @location = location
     @service = ClimbingRoutesService.new
   end
 
@@ -16,7 +18,7 @@ class ClimbingRoutesFacade
     route_array = response_routes[:routes]
 
     route_array.map do |route_info|
-      Route.new(route_info)
-    end 
+      Route.new(route_info, @location)
+    end
   end
 end
