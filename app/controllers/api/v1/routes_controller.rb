6ad49d6340fc9c7c79ce6b6cd149537cbcd49b8a
Sienna Kopf <princess.kopf@gmail.com>
2020-09-21 09:46:29 -0600

    Add functionality for transforming location into lat and long and collecting current weather for those coordinates from the forecast facade

--- app/controllers/api/v1/routes_controller.rb
+++ app/controllers/api/v1/routes_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::RoutesController < ApplicationController
   def index
+    location = params[:location]
+
+    lat_long = MapQuestFacade.new(location).lat_long
+    lat = lat_long.lat
+    lon = lat_long.lon
+
+    current_weather = ForecastFacade.new(lat, lon).current_forecast
     binding.pry
+
   end
 end
