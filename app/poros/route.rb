4641242a64832da179d138bc8fbf7958587ba9f3
Sienna Kopf <princess.kopf@gmail.com>
2020-09-21 10:40:15 -0600

    Add attribute distance to route for route object that calls the map quest service

--- app/poros/route.rb
+++ app/poros/route.rb
@@ -4,14 +4,16 @@ class Route
               :rating,
               :location,
               :latitude,
-              :longitude
+              :longitude,
+              :distance_to_route
 
-  def initialize(route_info)
+  def initialize(route_info, location)
     @name = route_info[:name]
     @type = route_info[:type]
     @rating = route_info[:rating]
     @location = route_info[:location]
     @latitude = route_info[:latitude]
     @longitude = route_info[:longitude]
+    @distance_to_route = MapQuestService.new.distance_to_route(location, @latitude, @longitude)[:route][:distance].round(2)
   end
 end
