f4e806755041596c62153d7be8407336a8d9e3fe
Sienna Kopf <princess.kopf@gmail.com>
2020-09-21 10:42:23 -0600

    Update climbing routes facade test to include another argument for location

--- spec/facades/climbing_routes_facade_spec.rb
+++ spec/facades/climbing_routes_facade_spec.rb
@@ -5,12 +5,13 @@ RSpec.describe ClimbingRoutesFacade do
     before :each do
       lat = 39.738453
       lon = -104.984853
-      @facade = ClimbingRoutesFacade.new(lat, lon)
+      location = "erwin, tn"
+      @facade = ClimbingRoutesFacade.new(lat, lon, location)
     end
     describe '#response_routes' do
       it "calls the mountain Project Api to return a list of routes near that location" do
         response = @facade.response_routes
-        
+
         expect(response).to be_a Hash
         expect(response[:routes]).to be_an Array
         target_route = response[:routes][0]
@@ -39,6 +40,7 @@ RSpec.describe ClimbingRoutesFacade do
         expect(response.location).to be_an Array
         expect(response.latitude).to be_a Float
         expect(response.longitude).to be_a Float
+        expect(response.distance_to_route).to be_a Float
       end
     end
   end
