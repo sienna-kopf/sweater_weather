e8a8345ec7a3cbb499c9ad63c3ddbdf5d84a0dd7
Sienna Kopf <princess.kopf@gmail.com>
2020-09-22 12:28:10 -0600

    Add long controller action for road trip controller that serializes for road trip

---
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::RoadTripController < ApplicationController
+  def show
+    if User.find_by(api_key: road_trip_params[:api_key])
+      origin = road_trip_params[:origin]
+      destination = road_trip_params[:destination]
+      trip = MapQuestFacade.new(origin, destination).trip
+
+      lat_long = MapQuestFacade.new(destination).lat_long
+      lat = lat_long.lat
+      lon = lat_long.lon
+
+      forecast = ForecastFacade.new(lat, lon).forecast_condensed(trip.travel_time)
+
+      render json: RoadTripSerializer.new(RoadTrip.new(origin, destination, trip.travel_time, forecast))
+    end
+  end
+
+  private
+
+  def road_trip_params
+    params.permit(:origin, :destination, :api_key)
+  end
+end
