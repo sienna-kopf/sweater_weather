19efdd7d75444ce460ef3bebf4912cfcd4162b76
Sienna Kopf <princess.kopf@gmail.com>
2020-09-22 10:49:30 -0600

    make small update to trip poro with travel time attribute

--- spec/poros/trip_spec.rb
+++ spec/poros/trip_spec.rb
@@ -10,6 +10,6 @@ describe Trip do
 
     expect(trip).to be_a Trip
     expect(trip.travel_time).to be_a String
-    expect(trip.travel_time).to eq("01:44")
+    expect(trip.travel_time).to eq("01:43:57")
   end
 end
