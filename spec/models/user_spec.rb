13dbf9e24635649421fa96a54c04a7e4d59dcc72
Sienna Kopf <princess.kopf@gmail.com>
2020-09-21 21:54:34 -0600

    Add validation for api_key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,6 @@ describe User, type: :model do
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
+    it {should validate_presence_of(:api_key)}
   end
 end
