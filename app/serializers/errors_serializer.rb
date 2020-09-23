4879d07133ca660ede4c795ab44819a0dbaa4755
Sienna Kopf <princess.kopf@gmail.com>
2020-09-22 19:25:43 -0600

    Add error serializer using fast json api

--- app/serializers/errors_serializer.rb
+++ app/serializers/errors_serializer.rb
@@ -1,22 +1,4 @@
 class ErrorsSerializer
-  def initialize(model)
-    @model = model
-  end
-
-  def errors_hash
-    if @model.errors.messages.empty?
-      error_messages = ["credentials are bad"]
-    else
-      error_messages = @model.errors.to_hash(true).map do |_, message|
-        message.join
-      end
-    end
-    json_hash = {
-      errors:
-      {
-        status: 400,
-        error_messages: error_messages
-      }
-    }
-  end
+  include FastJsonapi::ObjectSerializer
+  attributes :error_messages
 end
