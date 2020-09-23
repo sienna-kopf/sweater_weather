5eb79fd41fe143dfc584b6356f7aba2ce5d945cc
Sienna Kopf <princess.kopf@gmail.com>
2020-09-23 08:38:06 -0600

    final change

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by_email(user_params[:email])
-    if user.authenticate(user_params[:password])
-      session[:user_id] = user.id
-      render json: UsersSerializer.new(user)
+    if !user.nil?
+      if user.authenticate(user_params[:password])
+        session[:user_id] = user.id
+        render json: UsersSerializer.new(user)
+      else
+        render json: ErrorsSerializer.new(ErrorFacade.new(user).messages), status: :bad_request
+      end
     else
-      render json: ErrorsSerializer.new(ErrorFacade.new(user).messages), status: :bad_request
+      render json: {}, status: :bad_request
     end
   end
 
